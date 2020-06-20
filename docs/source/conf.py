8acd0bfbc086037545e343796e41a5351cb28cf3
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2020-06-20 13:30:31 -0700

    Bumped up version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,7 +65,7 @@ author = 'Rahul Bhadani'
 # built documents.
 #
 # The short X.Y version.
-version = u'0.3.0'
+version = u'0.3.7'
 # The full version, including alpha/beta/rc tags.
 release = u'beta'
 
